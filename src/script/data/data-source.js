5d099dad731631c74a91a57a167328fd8f8a6fde
Hadyan Ahmad <hadyandev@gmail.com>
2020-04-05 11:23:37 +0700

    15. Menerapkan Fetch dan Mengubah Data Source dari clubs.js menjadi TheSportsDb API

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,20 +1,18 @@
-import clubs from "./clubs.js";
-
 class DataSource {
-  //   remove constructor
-
-  // keyword static membuat method dari class bisa langsung diakses
   static searchClub(keyword) {
-    return new Promise((resolve, reject) => {
-      const filteredClubs = clubs.filter(club =>
-        club.name.toUpperCase().includes(keyword.toUpperCase())
-      );
-      if (filteredClubs.length) {
-        resolve(filteredClubs);
-      } else {
-        reject(`${keyword} is not found"`);
-      }
-    });
+    return fetch(
+      `https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${keyword}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJson) => {
+        if (responseJson.teams) {
+          return Promise.resolve(responseJson.teams);
+        } else {
+          return Promise.reject(`${keyword} is not found`);
+        }
+      });
   }
 }
 
